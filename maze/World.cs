58cfeff653590a7d713a8456ebae71433167d392
Lasse Lykke Jensen <lasselj@yandex.com>
2022-03-17 14:37:10 +0100

    Map loading in ResourceManager

--- maze/World.cs
+++ maze/World.cs
@@ -3,6 +3,7 @@ using textured_raycast.maze.math;
 using System;
 using rpg_game.maze.Fight;
 using textured_raycast.maze.sprites;
+using textured_raycast.maze.resources;
 using textured_raycast.maze.sprites.allSprites;
 
 namespace textured_raycast.maze
@@ -21,7 +22,7 @@ namespace textured_raycast.maze
     internal class World
     {
         public int currentMap = 1;
-        private Dictionary<int, Map> maps;
+        private Dictionary<int, string> maps;
 
         public states state = states.Game;
 
@@ -46,22 +47,23 @@ namespace textured_raycast.maze
 
         public World()
         {
-            maps = new Dictionary<int, Map>()
+            maps = new Dictionary<int, string>()
             {
-                {-1, new Map("maps/fightMap.map", this)},
-                // {1, new Map("maps/overworld.map", this)},
-                {1, new Map("maps/lightTest.map", this)},
-                // {2, new Map("maps/DarkDungeon.map", this)},
-                {3, new Map("maps/hiddenRoom.map", this)},
+                {-1, "maps/fightMap.map"},
+                // {1, "maps/overworld.map"},
+                {1, "maps/lightTest.map"},
+                // {2, "maps/DarkDungeon.map"},
+                {3, "maps/hiddenRoom.map"},
             };
 
-            plrPos = maps[currentMap].playerStartPos;
-            plrRot = maps[currentMap].playerStartRot;
+            Map curMap = ResourceManager.getMap(maps[currentMap], this);
+            plrPos = curMap.playerStartPos;
+            plrRot = curMap.playerStartRot;
         }
 
         public Map getMapByID(int id)
         {
-            return maps[id];
+            return ResourceManager.getMap(maps[id], this);
         }
     }
 }
