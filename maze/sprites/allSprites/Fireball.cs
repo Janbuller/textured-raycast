8d01a45bc541b5773291edaf130ef1e6f1b9b598
Lasse Lykke Jensen <lasselj@yandex.com>
2022-05-09 14:57:41 +0200

    Now fireballs start a fight when hitting an enemy

--- maze/sprites/allSprites/Fireball.cs
+++ maze/sprites/allSprites/Fireball.cs
@@ -10,6 +10,8 @@ namespace textured_raycast.maze.sprites.allSprites
         float intesity;
         Vector2d vel;
 
+        double size = 1;
+
         public Fireball(double posX, double posY, string[] texture, int effectID = 0, string whatsLeft = "") : base(posX, posY, texture, effectID, whatsLeft)
         {
             define(posX, posY, texture, effectID, whatsLeft);
@@ -23,15 +25,40 @@ namespace textured_raycast.maze.sprites.allSprites
             vel = new Vector2d(extraEffects[1]/100.0, extraEffects[2]/100.0);
         }
 
+	private Sprite CheckForCollision() {
+	    foreach(Sprite CheckSpr in World.getCurMap().sprites) {
+		if(CheckSpr == this)
+		    continue;
+
+		if(Math.Abs(CheckSpr.pos.DistTo(this.pos)) < size) {
+                    return CheckSpr;
+                }
+	    }
+
+            return null;
+        }
+
         public override void Update()
         {
             Map curMap = World.getCurMap();
             pos += vel * World.dt;
 
+	    bool HitEnemy = false;
+
+            Sprite HitSprite = CheckForCollision();
+            if (!(HitSprite is null))
+            {
+                if (typeof(Enemy) == HitSprite.GetType())
+                {
+                    HitEnemy = true;
+                    HitSprite.Activate();
+                }
+            }
+
             bool OutsideMap = pos.X > curMap.Width || pos.X < 0 || pos.Y > curMap.Height || pos.Y < 0;
             bool IsInWall   = World.getCurMap().IsWall((int)pos.X, (int)pos.Y);
 
-	    bool ShouldBeDeleted = OutsideMap || IsInWall;
+            bool ShouldBeDeleted = OutsideMap || IsInWall || HitEnemy;
 
             if(ShouldBeDeleted) {
                 World.getCurMap().sprites.Remove(this);
