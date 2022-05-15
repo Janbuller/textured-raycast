c709b73a62d906d0c0199183708db073b19c1240
Lasse Lykke Jensen <lasselj@yandex.com>
2022-05-15 15:18:34 +0200

    Add comments for skill.cs

--- game/maze/skills/Skill.cs
+++ game/maze/skills/Skill.cs
@@ -8,14 +8,20 @@ namespace textured_raycast.maze.skills
 {
     abstract class Skill
     {
+	// The name of the skill
         public string name;
+	// The id of the skills texture.
         public int TexID;
 
+	// Description of the skill
         public string Desc;
 
+	// Price of the skill
         public int price;
+	// The idx of all other skills, that need to be purchased first.
         public int[] requiredSkills;
 
+	// This skill id.
         public int id;
 
         public Skill(int id, int price, int[] requiredSkills)
@@ -25,27 +31,38 @@ namespace textured_raycast.maze.skills
             this.requiredSkills = requiredSkills;
         }
 
+	// Runs, when the player tries to buy the skill
         public void TryBuy()
         {
+	    // Can't buy, if it's too expensive.
             if (price > World.player.skillPoints)
                 return;
 
+	    // Can't buy if already owned.
             if (isBought())
                 return;
 
+	    // Whether all requiredskills are owned. True by default,
+	    // set to false if not.
             bool allOwned = true;
 
+	    // Reference to all unlocked skills.
             var UnlockedSkills = World.player.UnlockedSkills;
+	    // If this skill actually requires others to be purchased.
             if (requiredSkills != null)
             {
+		// Loop through each required skill
                 foreach (int skill in requiredSkills)
                 {
+		    // If the required skill is not in unlocked
+		    // skills, allowned is false.
                     if (!(UnlockedSkills.Contains(skill)))
                     {
                         allOwned = false;
                     }
                 }   
 
+		// Can't buy if requirements aren't met.
                 if (!allOwned)
                     return;
             }
@@ -61,10 +78,13 @@ namespace textured_raycast.maze.skills
             if (this is IPassiveSkillDON)
                 World.player.don = true;
 
+	    // Price is subtracted from the player and the skill is
+	    // added.
             World.player.skillPoints -= price;
             World.player.UnlockedSkills.Add(this.id);
         }
 
+	// Check if the skill is already bought.
         public bool isBought()
         {
             var UnlockedSkills = World.player.UnlockedSkills;
@@ -76,20 +96,31 @@ namespace textured_raycast.maze.skills
             return false;
         }
 
+	// Get the texture of the skill.
         public Texture getTexture()
         {
+	    // Poll the resourcemanager for the standard texture.
             Texture standardTex = ResourceManager.getTexture(SkillTextures[TexID]);
+
+	    //If the skill is already bought, return the standard
+	    //texture, otherwise, return a greyedout version.
             if (isBought())
             {
                 return standardTex;
             }
             else
             {
+		// Try to get the greyedout texture from the
+		// resourcemanager. If it exists, return it,
+		// otherwise, make it and cache it in the
+		// resourcemanager.
                 Texture greyedOut = ResourceManager.getTexture(SkillTextures[TexID] + "-progGrey");
                 if (greyedOut != null)
                     return greyedOut;
+
                 greyedOut = standardTex.getGreyscale();
                 ResourceManager.cacheTexture(SkillTextures[TexID] + "-progGrey", greyedOut);
+
                 return greyedOut;
             }
         }
@@ -102,6 +133,7 @@ namespace textured_raycast.maze.skills
             }
         }
 
+	// A list of all the textures for skills
         public static Dictionary<int, string> SkillTextures = new Dictionary<int, string>()
         {
             {1, "img/skills/tmp-slash-skill.ppm"},
